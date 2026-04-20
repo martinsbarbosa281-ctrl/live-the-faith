161adf650dd07026c9aac36278010eb9c4bfa5ea
martinsbarbosa281-ctrl <martinsbarbosa281@gmail.com>
2026-04-20 17:00:56 -0300

    Create listar.php

---
+++ listar.php
@@ -0,0 +1,25 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("localhost", "root", "Home@spSENAI2025!", "live_the_faith");
+
+if ($conn->connect_error) {
+    echo json_encode([]);
+    exit;
+}
+
+$sql = "SELECT * FROM produtos";
+$result = $conn->query($sql);
+
+$produtos = [];
+
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $produtos[] = $row;
+    }
+}
+
+echo json_encode($produtos);
+
+$conn->close();
+?>
