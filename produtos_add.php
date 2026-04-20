4a246abf7b674899eacf4abc1dc7e14cf4b3281b
martinsbarbosa281-ctrl <martinsbarbosa281@gmail.com>
2026-04-20 16:54:03 -0300

    Update produtos_add.php

--- produtos_add.php
+++ produtos_add.php
@@ -1,12 +1,44 @@
 <?php
+
+header("Content-Type: application/json; charset=UTF-8");
+
 $conn = new mysqli("localhost","root","Home@spSENAI2025!","live_the_faith");
 
-$nome = $_POST['nome'];
-$preco = $_POST['preco'];
-$imagem = $_POST['imagem'];
-$descricao = $_POST['descricao'];
+if ($conn->connect_error) {
+  echo json_encode(["status"=>"erro","mensagem"=>"Erro conexão"]);
+  exit;
+}
+
+$nome = $_POST['nome'] ?? '';
+$preco = $_POST['preco'] ?? 0;
+$imagem = $_POST['imagem'] ?? '';
+$descricao = $_POST['descricao'] ?? '';
+
+$preco = str_replace(",", ".", $preco);
 
 $stmt = $conn->prepare("INSERT INTO produtos (nome, preco, imagem, descricao) VALUES (?,?,?,?)");
+
+if(!$stmt){
+  echo json_encode(["status"=>"erro","mensagem"=>$conn->error]);
+  exit;
+}
+
 $stmt->bind_param("sdss", $nome, $preco, $imagem, $descricao);
-$stmt->execute();
+
+if(!$stmt->execute()){
+  echo json_encode([
+    "status"=>"erro",
+    "mensagem"=>$stmt->error
+  ]);
+  exit;
+}
+
+echo json_encode([
+  "status"=>"ok",
+  "mensagem"=>"Produto salvo com sucesso"
+]);
+
+$stmt->close();
+$conn->close();
+
 ?>
