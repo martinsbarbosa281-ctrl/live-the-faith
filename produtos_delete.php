6c67379dc06d9a495a277bab2ed93a64b15cac7e
martinsbarbosa281-ctrl <martinsbarbosa281@gmail.com>
2026-04-20 16:57:46 -0300

    Update produtos_delete.php

--- produtos_delete.php
+++ produtos_delete.php
@@ -1,7 +1,27 @@
 <?php
+
 $conn = new mysqli("localhost","root","Home@spSENAI2025!","live_the_faith");
 
-$id = $_GET['id'];
+if ($conn->connect_error) {
+  echo json_encode(["status"=>"erro","mensagem"=>"Erro na conexão"]);
+  exit;
+}
 
-$conn->query("DELETE FROM produtos WHERE id=$id");
+$id = $_GET['id'] ?? 0;
+
+// garante que é número
+$id = intval($id);
+
+$stmt = $conn->prepare("DELETE FROM produtos WHERE id = ?");
+$stmt->bind_param("i", $id);
+
+if($stmt->execute()){
+  echo json_encode(["status"=>"ok","mensagem"=>"Produto deletado"]);
+} else {
+  echo json_encode(["status"=>"erro","mensagem"=>"Erro ao deletar"]);
+}
+
+$stmt->close();
+$conn->close();
+
 ?>
